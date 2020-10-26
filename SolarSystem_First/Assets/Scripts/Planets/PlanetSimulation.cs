62b2e85da0a0f8d9afd2873ad7fe7c1816d94303
Tobias <tobias.forve@gmx.de>
2020-10-26 12:06:01 +0100

    Didn't changed a thing. now Calculation StartVelo correct
    - start Velocity of P1 now calculated finde

--- SolarSystem_First/Assets/Scripts/Planets/PlanetSimulation.cs
+++ SolarSystem_First/Assets/Scripts/Planets/PlanetSimulation.cs
@@ -24,6 +24,8 @@ public class PlanetSimulation : MonoBehaviour
 
     public PlanetBody[] planets;            // array to store all celestial bodies
 
+    public bool recalculateStartvelocity = false;
+
     private void Awake()
     {
         // populate array of planets
@@ -51,4 +53,21 @@ public class PlanetSimulation : MonoBehaviour
             planets[i].UpdatePosition(Universe.timeSteps);
         }
     }
+
+    /*
+    void OnValidate()
+    {
+        // calculate Startvelocity for debugging in Editor 
+        if(recalculateStartvelocity)
+        {
+            // populate planets array
+            planets = FindObjectsOfType<PlanetBody>();
+
+            for (int i = 0; i < planets.Length; i++)
+            {
+                planets[i].CalculateStartVelocity(planets);
+            }
+            recalculateStartvelocity = false;
+        }
+    }*/
 }
