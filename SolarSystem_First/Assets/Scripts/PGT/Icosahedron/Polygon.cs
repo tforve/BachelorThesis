70527870f753349700685c62699abd6c9bbbb668
Tobias <tobias.forve@gmx.de>
2020-11-16 22:37:36 +0100

    added Icosahedron Planet
    - added Icoplanet creater

---
+++ SolarSystem_First/Assets/Scripts/PGT/Icosahedron/Polygon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Polygon
+{
+    public List<int> m_Vertices;
+
+    public Polygon(int a, int b, int c)
+    {
+        m_Vertices = new List<int>() { a, b, c };
+    }
+
+}
