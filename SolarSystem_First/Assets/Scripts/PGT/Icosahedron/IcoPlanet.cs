b341929726fff2b340a31f207f83081d60be3e66
Forve <tobias.forve@gmx.de>
2020-11-20 15:42:40 +0100

    up

--- SolarSystem_First/Assets/Scripts/PGT/Icosahedron/IcoPlanet.cs
+++ SolarSystem_First/Assets/Scripts/PGT/Icosahedron/IcoPlanet.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class IcoPlanet : MonoBehaviour
 {
-    public Material m_Material;                 // material to apply
-    private GameObject m_PlanetMesh;            // mesh holder
+    public Material material;                 // material to apply
+    private GameObject planetMesh;            // mesh holder
 
-    private List<Polygon> m_Polygons;             // list of all Polygons
-    private List<Vector3> m_Vertices;             // list of all vertices
+    private List<Polygon> polygons;             // list of all Polygons
+    private List<Vector3> vertices;             // list of all vertices
 
-    public int m_Recursions;
+    [Header("Subdivition")]
+    [Range(0,5)][Tooltip("Unity has a default vertex limit on meshes of  5")]
+    public int subdivitions;
+
 
     void Start()
     {
         // instantiate Icosahedron 
         CreateIcosahedron();
         // subdivide in n amount of triangles
-        Subdivide(m_Recursions);
+        Subdivide(subdivitions);
         // generate correct Mesh
         GenerateMesh();
     }
@@ -26,49 +30,81 @@ public class IcoPlanet : MonoBehaviour
     // generate Icosahedron with 20 faces to subdivide later
     public void CreateIcosahedron()
     {
-        m_Polygons = new List<Polygon>();
-        m_Vertices = new List<Vector3>();
-
-        // Icosahedron has 12 vertices ...
-        // why this forumlar
+        polygons = new List<Polygon>();
+        vertices = new List<Vector3>();
+        
+        // t = golden Ratio 
         float t = (1.0f + Mathf.Sqrt(5.0f)) / 2.0f;
 
-        m_Vertices.Add(new Vector3(-1,  t,  0).normalized);
-        m_Vertices.Add(new Vector3( 1,  t,  0).normalized);
-        m_Vertices.Add(new Vector3(-1, -t,  0).normalized);
-        m_Vertices.Add(new Vector3( 1, -t,  0).normalized);
+        // cartesian coordinates, centered at the origin:
+        // (  0,+-1,+-t)
+        // (+-1,+-t,  0)
+        // (+-t,  0,+-1)
+        // Icosahedron has 12 vertices ...
+        vertices.Add(new Vector3(-1,  t,  0).normalized);
+        vertices.Add(new Vector3( 1,  t,  0).normalized);
+        vertices.Add(new Vector3(-1, -t,  0).normalized);
+        vertices.Add(new Vector3( 1, -t,  0).normalized);
 
-        m_Vertices.Add(new Vector3( 0, -1,  t).normalized);
-        m_Vertices.Add(new Vector3( 0,  1,  t).normalized);
-        m_Vertices.Add(new Vector3( 0, -1, -t).normalized);
-        m_Vertices.Add(new Vector3( 0,  1, -t).normalized);
+        vertices.Add(new Vector3( 0, -1,  t).normalized);
+        vertices.Add(new Vector3( 0,  1,  t).normalized);
+        vertices.Add(new Vector3( 0, -1, -t).normalized);
+        vertices.Add(new Vector3( 0,  1, -t).normalized);
 
-        m_Vertices.Add(new Vector3( t,  0, -1).normalized);
-        m_Vertices.Add(new Vector3( t,  0,  1).normalized);
-        m_Vertices.Add(new Vector3(-t,  0, -1).normalized);
-        m_Vertices.Add(new Vector3(-t,  0,  1).normalized);
+        vertices.Add(new Vector3( t,  0, -1).normalized);
+        vertices.Add(new Vector3( t,  0,  1).normalized);
+        vertices.Add(new Vector3(-t,  0, -1).normalized);
+        vertices.Add(new Vector3(-t,  0,  1).normalized);
 
-        // ... and 20 sides build out of thos 12 vertices
-        m_Polygons.Add(new Polygon( 0, 11,  5));
-        m_Polygons.Add(new Polygon( 0,  5,  1));
-        m_Polygons.Add(new Polygon( 0,  1,  7));
-        m_Polygons.Add(new Polygon( 0,  7, 10));
-        m_Polygons.Add(new Polygon( 0, 10, 11));
-        m_Polygons.Add(new Polygon( 1,  5,  9));
-        m_Polygons.Add(new Polygon( 5, 11,  4));
-        m_Polygons.Add(new Polygon(11, 10,  2));
-        m_Polygons.Add(new Polygon(10,  7,  6));
-        m_Polygons.Add(new Polygon( 7,  1,  8));
-        m_Polygons.Add(new Polygon( 3,  9,  4));
-        m_Polygons.Add(new Polygon( 3,  4,  2));
-        m_Polygons.Add(new Polygon( 3,  2,  6));
-        m_Polygons.Add(new Polygon( 3,  6,  8));
-        m_Polygons.Add(new Polygon( 3,  8,  9));
-        m_Polygons.Add(new Polygon( 4,  9,  5));
-        m_Polygons.Add(new Polygon( 2,  4, 11));
-        m_Polygons.Add(new Polygon( 6,  2, 10));
-        m_Polygons.Add(new Polygon( 8,  6,  7));
-        m_Polygons.Add(new Polygon( 9,  8,  1));
+        // trial ----------------- DELETE LATER ------------
+        //polygons.Add(new Polygon(0, 1, 2));
+        //polygons.Add(new Polygon(0, 2, 3));
+        //polygons.Add(new Polygon(0, 3, 4));
+        //polygons.Add(new Polygon(0, 4, 5));
+        //polygons.Add(new Polygon(0, 5, 1));
+
+        //polygons.Add(new Polygon(1, 2, 6));
+        //polygons.Add(new Polygon(2, 3, 7));
+        //polygons.Add(new Polygon(3, 4, 8));
+        //polygons.Add(new Polygon(4, 5, 9));
+        //polygons.Add(new Polygon(5, 1, 10));
+
+        //polygons.Add(new Polygon(11, 6, 7));
+        //polygons.Add(new Polygon(11, 7, 8));
+        //polygons.Add(new Polygon(11, 8, 9));
+        //polygons.Add(new Polygon(11, 9, 10));
+        //polygons.Add(new Polygon(11, 10, 6));
+
+        //polygons.Add(new Polygon(7, 8, 3));
+        //polygons.Add(new Polygon(8, 9, 4));
+        //polygons.Add(new Polygon(9, 10, 5));
+        //polygons.Add(new Polygon(10, 6, 1));
+        //polygons.Add(new Polygon(6, 7, 2));
+
+        // ... and 20 faces build out of thos 12 vertices
+        polygons.Add(new Polygon( 0, 11,  5));
+        polygons.Add(new Polygon( 0,  5,  1));
+        polygons.Add(new Polygon( 0,  1,  7));
+        polygons.Add(new Polygon( 0,  7, 10));
+        polygons.Add(new Polygon( 0, 10, 11));
+
+        polygons.Add(new Polygon( 1,  5,  9));
+        polygons.Add(new Polygon( 5, 11,  4));
+        polygons.Add(new Polygon(11, 10,  2));
+        polygons.Add(new Polygon(10,  7,  6));
+        polygons.Add(new Polygon( 7,  1,  8));
+        //second half of icosahedron
+        polygons.Add(new Polygon( 3,  9,  4));
+        polygons.Add(new Polygon( 3,  4,  2));
+        polygons.Add(new Polygon( 3,  2,  6));
+        polygons.Add(new Polygon( 3,  6,  8));
+        polygons.Add(new Polygon( 3,  8,  9));
+
+        polygons.Add(new Polygon( 4,  9,  5));
+        polygons.Add(new Polygon( 2,  4, 11));
+        polygons.Add(new Polygon( 6,  2, 10));
+        polygons.Add(new Polygon( 8,  6,  7));
+        polygons.Add(new Polygon( 9,  8,  1));
     }
 
 
@@ -77,10 +113,17 @@ public class IcoPlanet : MonoBehaviour
     {
         var midPointCache = new Dictionary<int, int>();
 
+        if(recursions < 0 || recursions > 5)
+        {
+            //recursions = Mathf.Clamp(recursions, 0, 5);
+            Debug.Log("Unity has a default vertex limit on meshes of 5");
+            return;
+        }
+
         for (int i = 0; i < recursions; i++)
         {
             var newPolys = new List<Polygon>();
-            foreach (var poly in m_Polygons)
+            foreach (var poly in polygons)
             {
                 int a = poly.m_Vertices[0];
                 int b = poly.m_Vertices[1];
@@ -99,7 +142,7 @@ public class IcoPlanet : MonoBehaviour
                 newPolys.Add(new Polygon(ab, bc, ca));
             }
             // Replace all our old polygons with the new set of subdivided ones.
-            m_Polygons = newPolys;
+            polygons = newPolys;
         }
 
 
@@ -130,12 +173,12 @@ public class IcoPlanet : MonoBehaviour
         // If we're here, it's because a midpoint for these two
         // vertices hasn't been created yet. Let's do that now!
 
-        Vector3 p1 = m_Vertices[indexA];
-        Vector3 p2 = m_Vertices[indexB];
+        Vector3 p1 = vertices[indexA];
+        Vector3 p2 = vertices[indexB];
         Vector3 middle = Vector3.Lerp(p1, p2, 0.5f).normalized;
 
-        ret = m_Vertices.Count;
-        m_Vertices.Add(middle);
+        ret = vertices.Count;
+        vertices.Add(middle);
 
         // Add our new midpoint to the cache so we don't have
         // to do this again. =)
@@ -148,18 +191,18 @@ public class IcoPlanet : MonoBehaviour
     public void GenerateMesh()
     {
         // check if existing. if so destroy and recreate
-        if (m_PlanetMesh)
-            Destroy(m_PlanetMesh);
+        if (planetMesh)
+            Destroy(planetMesh);
 
-        m_PlanetMesh = new GameObject("Planet Mesh");
+        planetMesh = new GameObject("Planet Mesh");
 
         // add necessary components
-        MeshRenderer surfaceRenderer = m_PlanetMesh.AddComponent<MeshRenderer>();
-        surfaceRenderer.material = m_Material;
+        MeshRenderer surfaceRenderer = planetMesh.AddComponent<MeshRenderer>();
+        surfaceRenderer.material = material;
         Mesh terrainMesh = new Mesh();
 
 
-        int vertexCount = m_Polygons.Count * 3;
+        int vertexCount = polygons.Count * 3;
 
         int[] indices = new int[vertexCount];
 
@@ -171,17 +214,17 @@ public class IcoPlanet : MonoBehaviour
         Color32 brown = new Color32(220, 150, 70, 255);
 
         // loop through all Polygons
-        for (int i = 0; i < m_Polygons.Count; i++)
+        for (int i = 0; i < polygons.Count; i++)
         {
-            var poly = m_Polygons[i];
+            var poly = polygons[i];
 
             indices[i * 3 + 0] = i * 3 + 0;
             indices[i * 3 + 1] = i * 3 + 1;
             indices[i * 3 + 2] = i * 3 + 2;
 
-            vertices[i * 3 + 0] = m_Vertices[poly.m_Vertices[0]];
-            vertices[i * 3 + 1] = m_Vertices[poly.m_Vertices[1]];
-            vertices[i * 3 + 2] = m_Vertices[poly.m_Vertices[2]];
+            vertices[i * 3 + 0] = this.vertices[poly.m_Vertices[0]];
+            vertices[i * 3 + 1] = this.vertices[poly.m_Vertices[1]];
+            vertices[i * 3 + 2] = this.vertices[poly.m_Vertices[2]];
 
             Color32 polyColor = Color32.Lerp(green, brown, Random.Range(0.0f, 1.0f)); 
 
@@ -189,9 +232,9 @@ public class IcoPlanet : MonoBehaviour
             colors[i * 3 + 1] = polyColor;
             colors[i * 3 + 2] = polyColor;
 
-            normals[i * 3 + 0] = m_Vertices[poly.m_Vertices[0]];
-            normals[i * 3 + 1] = m_Vertices[poly.m_Vertices[1]];
-            normals[i * 3 + 2] = m_Vertices[poly.m_Vertices[2]];
+            normals[i * 3 + 0] = this.vertices[poly.m_Vertices[0]];
+            normals[i * 3 + 1] = this.vertices[poly.m_Vertices[1]];
+            normals[i * 3 + 2] = this.vertices[poly.m_Vertices[2]];
         }
 
         terrainMesh.vertices = vertices;
@@ -200,7 +243,7 @@ public class IcoPlanet : MonoBehaviour
 
         terrainMesh.SetTriangles(indices, 0);
 
-        MeshFilter terrainFilter = m_PlanetMesh.AddComponent<MeshFilter>();
+        MeshFilter terrainFilter = planetMesh.AddComponent<MeshFilter>();
         terrainFilter.mesh = terrainMesh;
     }
 }
