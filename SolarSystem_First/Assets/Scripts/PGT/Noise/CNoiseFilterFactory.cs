e4fae0a89a91e94f4b5a46e45f7162a92106c776
Forve <tobias.forve@gmx.de>
2020-12-20 15:10:39 +0100

    Save bevor Renaming

---
+++ SolarSystem_First/Assets/Scripts/PGT/Noise/CNoiseFilterFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CNoiseFilterFactory
+{
+    public static INoiseFilter CreateNoiseFilter(CNoiseSettings settings)
+    {
+        switch (settings.filterType)
+        {
+            case CNoiseSettings.FilterType.Simple:
+                return new CSimpleNoiseFilter(settings.stdNoiseSettings);
+            case CNoiseSettings.FilterType.Ridgid:
+                return new CRidgidNoiseFilter(settings.ridgidNoiseSettings);
+            case CNoiseSettings.FilterType.Hilly:
+                return new CHillyNoiseFilter(settings.stdNoiseSettings);
+            case CNoiseSettings.FilterType.Brain:
+                return new CBrainNoiseFilter(settings.stdNoiseSettings);
+            default:
+                break;
+        }
+        return null;
+    }
+}
---
+++ SolarSystem_First/Assets/Scripts/PGT/Noise/CSimpleNoiseFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSimpleNoiseFilter : INoiseFilter
+{
+    CNoiseSettings.StdNoiseSettings settings;
+    Noise noise = new Noise();
+
+    public CSimpleNoiseFilter(CNoiseSettings.StdNoiseSettings settings)
+    {
+        this.settings = settings;
+    }
+
+    // calculate noise by using the Noise class
+    public float Evaluate(Vector3 point)
+    {
+        // final noiseValue
+        float noiseValue = 0;
+        // setting frequency and amplitude to define layers of noise
+        float frequency = settings.baseRoughness;
+        float amplitude = 1;
+
+        for (int i = 0; i < settings.numberOfLayers; i++)
+        {
+            float v = noise.Evaluate(point * frequency + settings.centre);
+            // change range from -1/1 to 0/1
+            noiseValue += (v + 1) * 0.5f * amplitude;
+            frequency *= settings.roughness;    // roughness > 1 = frequency will increase with each layer
+            amplitude *= settings.persistence;  // persistence < 1 = amplitude will decrease with each layer
+        }
+        noiseValue = noiseValue - settings.seaLevel;
+        return noiseValue * settings.strength;
+    }
+}
